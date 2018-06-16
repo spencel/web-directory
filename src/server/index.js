f82bb9701a24c4769b2aa6dfbd220f2a801b5f2a
spencel <spence.lank@gmail.com>
2018-06-15 20:28:52 -0700

    m

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@
 
 import express from 'express';
 import os from 'os';
-import mongodb from  'mongodb';
+import mongodb from 'mongodb';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -23,53 +23,62 @@ var mongoDbName = 'web-directory';
 var mongoDbUrl = `mongodb://${mongoDbUserName}:${mongoDbUserPassword}@ds123728.mlab.com:23728/${mongoDbName}`;
 console.log( mongoDbUrl );
 var mongo = undefined;
-mongodb.MongoClient.connect( mongoDbUrl, ( error, client ) => {
-	if ( error ) { console.error( error ); return; }
-	console.log( 'connection successful' );
-	mongo = {
-		client: client,
-		db: client.db( mongoDbName )
-	};
-});
+mongodb.MongoClient.connect(
+  mongoDbUrl,
+  ( error, client ) => {
+    if ( error ) {
+      console.error( error );
+      return;
+    }
+    console.log( 'connection successful' );
+    mongo = {
+      client: client,
+      db: client.db( mongoDbName )
+    };
+  }
+);
 
 // Database Collection Names
 const categoriesCollectionName = 'categories';
 
 // End Database
 
-
 // Server
 
 app.get( '/api/test', ( request, response ) => {
-	console.log( 'starting /api/test' );
-	mongo.db.listCollections().toArray(( error, result ) => {
-		if ( error ) { console.error( error ); return; }
-		response.send({ 
-			username: os.userInfo().username,
-			result: result
-		});
-	});
+  console.log( 'starting /api/test' );
+  mongo.db.listCollections().toArray(( error, result ) => {
+    if ( error ) {
+      console.error( error );
+      return;
+    }
+    response.send({
+      username: os.userInfo().username,
+      result: result
+    });
+  });
 });
 
 app.get( '/api/getCategories', ( request, response ) => {
-	mongo.db.collection( categoriesCollectionName )
-	.find({}, null )
-	.toArray(( error, documents ) => {
-		response.send( documents );
-	});
+  mongo.db
+    .collection( categoriesCollectionName )
+    .find({}, null )
+    .toArray(( error, documents ) => {
+      response.send( documents );
+    });
 });
 
 app.post( '/api/createCategory', jsonParser, ( request, response ) => {
-	console.log( 'request:' );
-	console.log( request );
-	console.log( 'request.body:' );
-	console.log( request.body );
-	mongo.db.collection( categoriesCollectionName )
-	.insertOne({ name: request.body.categoryName }, null, ( error, result ) => {
-		console.log( result.ops[ 0 ] );
-		response.send( JSON.stringify( result.ops[ 0 ] ));
-	});
+  console.log( 'request:' );
+  console.log( request );
+  console.log( 'request.body:' );
+  console.log( request.body );
+  mongo.db
+    .collection( categoriesCollectionName )
+    .insertOne({ name: request.body.categoryName }, null, ( error, result ) => {
+      console.log( result.ops[0] );
+      response.send( JSON.stringify( result.ops[0] ));
+    });
 });
-
 
 app.listen( 8080, () => console.log( 'Listening on port 8080!' ));
